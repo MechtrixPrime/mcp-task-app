649d6964b39284007a455146b85a8dfa84512a07
MechtrixPrime <black2knightshield@gmail.com>
2026-05-27 16:35:46 +0800

    feat: added km to miles converter

--- app/mcp/mcp_tools/conversion.py
+++ app/mcp/mcp_tools/conversion.py
@@ -1,3 +1,6 @@
 
 def miles_to_kilometers_converter(miles: float) -> float:
     return miles / 0.621371
+
+def kilometers_to_miles_converter(kilometers: float) -> float:
+    return kilometers * 0.621371
